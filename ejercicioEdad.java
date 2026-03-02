5882b13c5798474cc2f4b9bc3819ba030983b9df
Sandra Mira <sandramiramartin@gmail.com>
2026-03-02 10:40:12 +0100

    Ejercicio para explicar en clase, pone el enunciado

---
+++ ejercicioEdad.java
@@ -0,0 +1,15 @@
+//El publicClass debe tener el mismo nombre que el archivo
+
+public class ejercicioEdad {
+
+    /*"Crea un programa que tenga un array con las edades de 4 personas, y luego calcule y muestre
+    la edad media."*/
+    //creamos nuestro metodo main es necesario para ejecutar el programa
+    public static void main(String[] args) {
+        //Crea las variables necesarias
+
+        //Calcula la suma de las edades
+        //Haz la media de las edades
+        //Imprime el resultado por pantalla
+    }
+}
