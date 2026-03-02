decc65d428bc41bcfb717114d99e1da073df87a9
Sandra Mira <sandramiramartin@gmail.com>
2026-03-02 10:32:03 +0100

    Media con numeros random

--- EdadMedia.java
+++ EdadMedia.java
@@ -1,16 +1,23 @@
+
 public class EdadMedia {
 
     public static void main(String[] args) {
 
-        int[] edades = {20, 22, 25, 23};
+        int[] edades = new int[5];
         int suma = 0;
 
-        for (int edad : edades) {
-            suma += edad;
+        for (int i = 0; i < edades.length; i++) {
+            edades[i] = (int) (Math.random() * 100); // Genera edades aleatorias entre 0 y 99
+            suma += edades[i];
         }
 
         double media = (double) suma / edades.length;
 
-        System.out.println("La edad media es: " + media);
+        System.out.println("Edades generadas:");
+        for (int edad : edades) {
+            System.out.print(edad + "/");
+        }
+
+        System.out.println("\nEdad media: " + media);
     }
 }
